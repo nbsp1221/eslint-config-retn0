f831555c3966a60e462b31f82db552527d2b2afe
Naki <nbsp1221@gmail.com>
2022-07-25 00:00:02 +0900

    🚚 Move ESLint configuration from .eslintrc.js to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-module.exports = require('./.eslintrc.js');
+const javascriptRules = require('./rules/javascript');
+const typescriptRules = require('./rules/typescript');
+const reactRules = require('./rules/react');
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['eslint-plugin-react'],
+  rules: {
+    ...javascriptRules,
+    ...reactRules,
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: typescriptRules,
+    },
+  ],
+};
