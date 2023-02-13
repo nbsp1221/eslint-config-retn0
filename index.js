d1ecfd8593ac539f29db02253aa8b6ff4b600caf
Naki <nbsp1221@gmail.com>
2023-02-13 16:23:10 +0900

    🐛 Fix bug causing crashes when not using React

--- index.js
+++ index.js
@@ -3,22 +3,51 @@ const javascriptRules = require('./rules/javascript');
 const reactRules = require('./rules/react');
 const typescriptRules = require('./rules/typescript');
 
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  plugins: ['eslint-plugin-react', 'import'],
-  rules: {
-    ...javascriptRules,
-    ...reactRules,
-    ...importRules,
-  },
-  overrides: [
-    {
+function isInstalled(name) {
+  try {
+    if (require.resolve(name)) {
+      return true;
+    }
+
+    return false;
+  }
+  catch {
+    return false;
+  }
+}
+
+function createConfig() {
+  const config = {
+    parserOptions: {
+      ecmaVersion: 'latest',
+    },
+    plugins: [],
+    rules: {
+      ...javascriptRules,
+    },
+    overrides: [],
+  };
+
+  if (isInstalled('typescript')) {
+    config.overrides.push({
       files: ['*.ts', '*.tsx'],
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint'],
       rules: typescriptRules,
-    },
-  ],
-};
+    });
+  }
+
+  if (isInstalled('eslint-plugin-import')) {
+    config.plugins.push('eslint-plugin-import');
+    config.rules = { ...config.rules, ...importRules };
+  }
+
+  if (isInstalled('eslint-plugin-react')) {
+    config.plugins.push('eslint-plugin-react');
+    config.rules = { ...config.rules, ...reactRules };
+  }
+
+  return config;
+}
+
+module.exports = createConfig();
