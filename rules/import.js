d32ea357af6784f869a1f3738445362b4e220217
Naki <nbsp1221@gmail.com>
2023-03-23 01:23:21 +0900

    ✨ Update eslint-plugin-import rules

--- rules/import.js
+++ rules/import.js
@@ -2,13 +2,6 @@ const importRules = {
   'import/first': ['error'],
   'import/no-duplicates': ['error'],
   'import/no-self-import': ['error'],
-  'import/order': ['error', {
-    groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
-    alphabetize: {
-      order: 'asc',
-      caseInsensitive: true,
-    },
-  }],
 };
 
 module.exports = importRules;
